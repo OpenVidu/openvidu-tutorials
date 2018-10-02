e999e7cd7a0a8ce943124d57e3878dbc55e28867
pabloFuente <pablofuenteperez@gmail.com>
2018-10-02 10:20:22 +0200

    openvidu-tutorials update to 2.5.0

--- openvidu-js-java/src/main/java/io/openvidu/js/java/SessionController.java
+++ openvidu-js-java/src/main/java/io/openvidu/js/java/SessionController.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduRole;
 import io.openvidu.java.client.Session;
 import io.openvidu.java.client.TokenOptions;
-import io.openvidu.java.client.OpenViduRole;
 
 @RestController
 @RequestMapping("/api-sessions")
@@ -30,7 +30,8 @@ public class SessionController {
 
 	// Collection to pair session names and OpenVidu Session objects
 	private Map<String, Session> mapSessions = new ConcurrentHashMap<>();
-	// Collection to pair session names and tokens (the inner Map pairs tokens and role associated)
+	// Collection to pair session names and tokens (the inner Map pairs tokens and
+	// role associated)
 	private Map<String, Map<String, OpenViduRole>> mapSessionNamesTokens = new ConcurrentHashMap<>();
 
 	// URL where our OpenVidu server is listening
@@ -38,7 +39,6 @@ public class SessionController {
 	// Secret shared with our OpenVidu server
 	private String SECRET;
 
-	
 	public SessionController(@Value("${openvidu.secret}") String secret, @Value("${openvidu.url}") String openviduUrl) {
 		this.SECRET = secret;
 		this.OPENVIDU_URL = openviduUrl;
@@ -57,15 +57,16 @@ public class SessionController {
 		System.out.println("Getting a token from OpenVidu Server | {sessionName}=" + sessionNameParam);
 
 		JSONObject sessionJSON = (JSONObject) new JSONParser().parse(sessionNameParam);
-	
+
 		// The video-call to connect
 		String sessionName = (String) sessionJSON.get("sessionName");
-		
+
 		// Role associated to this user
 		OpenViduRole role = LoginController.users.get(httpSession.getAttribute("loggedUser")).role;
-		
-		// Optional data to be passed to other users when this user connects to the video-call
-		// In this case, a JSON with the value we stored in the HttpSession object on login
+
+		// Optional data to be passed to other users when this user connects to the
+		// video-call. In this case, a JSON with the value we stored in the HttpSession
+		// object on login
 		String serverData = "{\"serverData\": \"" + httpSession.getAttribute("loggedUser") + "\"}";
 
 		// Build tokenOptions object with the serverData and the role
@@ -77,19 +78,19 @@ public class SessionController {
 			// Session already exists
 			System.out.println("Existing session " + sessionName);
 			try {
-			
+
 				// Generate a new token with the recently created tokenOptions
 				String token = this.mapSessions.get(sessionName).generateToken(tokenOptions);
-				
+
 				// Update our collection storing the new token
 				this.mapSessionNamesTokens.get(sessionName).put(token, role);
-				
+
 				// Prepare the response with the token
 				responseJson.put(0, token);
-				
+
 				// Return the response to the client
 				return new ResponseEntity<>(responseJson, HttpStatus.OK);
-				
+
 			} catch (Exception e) {
 				// If error generate an error message and return it to client
 				return getErrorResponse(e);
@@ -115,7 +116,7 @@ public class SessionController {
 
 				// Return the response to the client
 				return new ResponseEntity<>(responseJson, HttpStatus.OK);
-				
+
 			} catch (Exception e) {
 				// If error generate an error message and return it to client
 				return getErrorResponse(e);
@@ -141,7 +142,7 @@ public class SessionController {
 
 		// If the session exists
 		if (this.mapSessions.get(sessionName) != null && this.mapSessionNamesTokens.get(sessionName) != null) {
-			
+
 			// If the token exists
 			if (this.mapSessionNamesTokens.get(sessionName).remove(token) != null) {
 				// User left the session
@@ -155,7 +156,7 @@ public class SessionController {
 				System.out.println("Problems in the app server: the TOKEN wasn't valid");
 				return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 			}
-			
+
 		} else {
 			// The SESSION does not exist
 			System.out.println("Problems in the app server: the SESSION does not exist");
