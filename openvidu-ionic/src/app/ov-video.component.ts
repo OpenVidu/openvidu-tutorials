03ed6343d994e2dea42984f932cc3757d149563c
pabloFuente <pablofuenteperez@gmail.com>
2018-12-20 12:38:04 +0100

    openvidu-ionic: redundant lines removed

--- openvidu-ionic/src/app/ov-video.component.ts
+++ openvidu-ionic/src/app/ov-video.component.ts
@@ -22,7 +22,6 @@ export class OpenViduVideoComponent implements AfterViewInit, OnDestroy {
                 // Give the remote video some time to update its dimensions when rotating the device
                 setTimeout(() => {
                     this.applyIosIonicVideoAttributes();
-                    cordova.plugins.iosrtc.refreshVideos();
                 }, 250);
             };
             (<any>window).addEventListener('orientationchange', this.rotationFunction);
@@ -43,7 +42,6 @@ export class OpenViduVideoComponent implements AfterViewInit, OnDestroy {
             this._streamManager.on('streamPropertyChanged', event => {
                 if ((<StreamPropertyChangedEvent>event).changedProperty === 'videoDimensions') {
                     this.applyIosIonicVideoAttributes();
-                    cordova.plugins.iosrtc.refreshVideos();
                 }
             });
         }
