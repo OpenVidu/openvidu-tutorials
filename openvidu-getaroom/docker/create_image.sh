c9843ac2b38b3c14a6caf5c2a1ff1545aa85efcc
pabloFuente <pablofuenteperez@gmail.com>
2019-01-10 14:19:39 +0100

    ngrok removed from docker containers (getaroom, insecure-js, js-java)

--- openvidu-getaroom/docker/create_image.sh
+++ openvidu-getaroom/docker/create_image.sh
@@ -4,9 +4,6 @@ cp ../../../openvidu/openvidu-server/target/openvidu-server-"$1".jar ./openvidu-
 # Copy openvidu-insecure-js web files
 cp -a ../web/. ./web/
 
-# Modify OpenVidu Server URL
-sed -i 's/var OPENVIDU_SERVER_URL = "https:\/\/" + location\.hostname + ":4443";/var OPENVIDU_SERVER_URL = "https:\/\/" + location\.hostname;/g' ./web/app.js
-
 # Build docker image
 docker build -t openvidu/getaroom-demo .
 
--- openvidu-insecure-js/docker/ngrok.sh
+++
@@ -1,6 +0,0 @@
-exec $HOME/ngrok start --all -config=$HOME/ngrok.yml > $HOME/ngrok.log &
-while true
-do
-	sleep 100000
-done
-exit 0
