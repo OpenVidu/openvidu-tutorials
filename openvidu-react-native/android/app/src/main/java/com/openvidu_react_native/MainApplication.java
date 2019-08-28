b9eb866522f451306ccc78902d9b8a508b79f23b
CSantos <4a.santos@gmail.com>
2019-08-28 15:44:38 +0200

    openvidu-react-native updated to RN 60.5

--- openvidu-react-native/android/app/src/main/java/com/openvidu_react_native/MainApplication.java
+++ openvidu-react-native/android/app/src/main/java/com/openvidu_react_native/MainApplication.java
@@ -1,16 +1,20 @@
 package com.openvidu_react_native;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.facebook.react.PackageList;
+import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
+import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
+import java.util.List;
+
 import com.oney.WebRTCModule.WebRTCModulePackage;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,10 +26,12 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new WebRTCModulePackage() 
-      );
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      packages.add(new WebRTCModulePackage());
+      return packages;
     }
 
     @Override
