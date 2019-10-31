01eb95591c851b938bf5115d07fe0a220ed88266
pabloFuente <pablofuenteperez@gmail.com>
2019-10-31 14:31:04 +0100

    openvidu-ipcameras

---
+++ openvidu-ipcameras/src/main/java/io/openvidu/ipcameras/App.java
@@ -0,0 +1,60 @@
+package io.openvidu.ipcameras;
+
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Run this application with the following command:
+ * 
+ * java -jar -Dopenvidu-url=https://your.url.com -Dopenvidu-secret=your_secret openvidu-ipcameras-1.0.0.jar
+ * 
+ * @author Pablo Fuente (pablofuenteperez@gmail.com)
+ */
+@SpringBootApplication
+@PropertySource("classpath:application.properties")
+public class App {
+
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+
+	static String OPENVIDU_URL;
+	static String OPENVIDU_SECRET;
+	static Map<String, String> IP_CAMERAS = new HashMap<String, String>() {
+		{
+			put("Beach camera 1", "rtsp://b1.dnsdojo.com:1935/live/sys3.stream");
+			put("Beach camera 2", "rtsp://b1.dnsdojo.com:1935/live/sys5.stream");
+			put("City", "rtsp://91.191.213.49:554/live_mpeg4.sdp");
+		}
+	};
+
+	public static void main(String[] args) {
+
+		// Start application
+		ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+
+		OPENVIDU_URL = context.getEnvironment().getProperty("openvidu-url");
+		OPENVIDU_SECRET = context.getEnvironment().getProperty("openvidu-secret");
+
+		// Check OPENVIDU_URL parameter
+		try {
+			new URL(OPENVIDU_URL).toURI();
+			OPENVIDU_URL = OPENVIDU_URL.endsWith("/") ? OPENVIDU_URL : (OPENVIDU_URL + "/");
+		} catch (MalformedURLException | URISyntaxException e) {
+			log.error("Parameter \"openvidu-url\" ({}) has not a valid URL format", OPENVIDU_URL);
+			System.exit(1);
+		}
+		log.info("OpenVidu URL is {}", OPENVIDU_URL);
+		log.info("OpenVidu secret is {}", OPENVIDU_SECRET);
+		log.info("Camera list is {}", IP_CAMERAS.toString());
+	}
+
+}
---
+++ openvidu-ipcameras/src/main/java/io/openvidu/ipcameras/MyRestController.java
@@ -0,0 +1,135 @@
+package io.openvidu.ipcameras;
+
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.openvidu.java.client.OpenVidu;
+import io.openvidu.java.client.OpenViduHttpException;
+import io.openvidu.java.client.OpenViduJavaClientException;
+import io.openvidu.java.client.Session;
+import io.openvidu.java.client.SessionProperties;
+
+/**
+ * Rest controller that offers a single entry point ("/"), where users can
+ * request for a token to enter the OpenVidu session if their credentials are
+ * right. First time a user provides the required credentials, the OpenVidu
+ * session will be created and the cameras will be published just before
+ * generating and returning the user's token
+ * 
+ * @author Pablo Fuente (pablofuenteperez@gmail.com)
+ */
+@Controller
+public class MyRestController {
+
+	private static final Logger log = LoggerFactory.getLogger(App.class);
+
+	private final String USER_CREDENTIALS = "PASSWORD";
+	private final String SESSION_ID = "MySurveillanceSession";
+
+	// OpenVidu objects
+	private OpenVidu OV;
+	private Session session;
+
+	// A simple HTTP client to perform OpenVidu REST API operations that are not
+	// available yet in OpenVidu Java Client
+	private final SimpleHttpClient httpClient = new SimpleHttpClient();
+
+	@RequestMapping(value = "/")
+	public String subscribe(@RequestParam(name = "credentials", required = false) String credentials, Model model) {
+
+		try {
+			checkCredentials(credentials);
+		} catch (Exception e) {
+			return generateError(model, "Wrong credentials");
+		}
+
+		// Create our surveillance session if not available yet
+		if (OV == null || session == null) {
+			try {
+				createOpenViduSession();
+				publishCameras();
+			} catch (OpenViduJavaClientException | OpenViduHttpException e) {
+				return generateError(model,
+						"Error sending request to OpenVidu Server: " + e.getCause() + ". " + e.getMessage());
+			}
+		}
+
+		// Generate a token for the user
+		String token = null;
+		try {
+			token = this.session.generateToken();
+		} catch (OpenViduJavaClientException | OpenViduHttpException e) {
+			return generateError(model,
+					"Error creating OpenVidu token for session " + SESSION_ID + ": " + e.getMessage());
+		}
+
+		model.addAttribute("token", token);
+		return "index";
+	}
+
+	private void createOpenViduSession() throws OpenViduJavaClientException, OpenViduHttpException {
+		// Init OpenVidu entrypoint object
+		OV = new OpenVidu(App.OPENVIDU_URL, App.OPENVIDU_SECRET);
+		// Get active sessions from OpenVidu Server
+		OV.fetch();
+		try {
+			// See if our surveillance session is already created in OpenVidu Server
+			session = OV.getActiveSessions().stream().filter(s -> s.getSessionId().equals(SESSION_ID)).findFirst()
+					.get();
+			log.info("Session {} already existed in OpenViduU Server", SESSION_ID);
+		} catch (NoSuchElementException e) {
+			// Create our surveillance session if it does not exist yet in OpenVidu Server
+			log.info("Session {} does not in OpenViduU Servery yet. Creating it...", SESSION_ID);
+			SessionProperties properties = new SessionProperties.Builder().customSessionId(SESSION_ID).build();
+			session = OV.createSession(properties);
+			log.info("Session {} created", SESSION_ID);
+		}
+	}
+
+	private void publishCameras() throws OpenViduJavaClientException, OpenViduHttpException {
+
+		// See if we have already published any of our cameras
+		// We fetch our only session current status and search for connections with
+		// platform "IPCAM". Finally we get their server data field with the camera name
+		session.fetch();
+		List<String> alreadyPublishedCameras = session.getActiveConnections().stream()
+				.filter(connection -> "IPCAM".equals(connection.getPlatform()))
+				.map(connection -> connection.getServerData()).collect(Collectors.toList());
+
+		for (Entry<String, String> cameraMapEntry : App.IP_CAMERAS.entrySet()) {
+			try {
+				String cameraUri = cameraMapEntry.getValue();
+				String cameraName = cameraMapEntry.getKey();
+				if (!alreadyPublishedCameras.contains(cameraName)) {
+					// Publish the camera only if it is not already published
+					httpClient.publishIpCamera(SESSION_ID, cameraUri, cameraName, true, true);
+				}
+			} catch (Exception e) {
+				log.error("Error publishing camera {}", cameraMapEntry.getKey());
+			}
+		}
+	}
+
+	private void checkCredentials(String credentials) throws Exception {
+		// Dummy security: if not expected string, then throw error
+		if (!credentials.equals(USER_CREDENTIALS)) {
+			throw new Exception();
+		}
+	}
+
+	private String generateError(Model model, String message) {
+		log.error(message);
+		model.addAttribute("error", message);
+		return "index";
+	}
+
+}
