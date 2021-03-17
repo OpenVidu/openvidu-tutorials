6723e662ec762eb8e589ece084b4ae3c5c4f2047
pabloFuente <pablofuenteperez@gmail.com>
2021-03-17 12:39:46 +0100

    Mediasoup refactoring

--- openvidu-react-native/index.js
+++ openvidu-react-native/index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import 'react-native-get-random-values';
 
 AppRegistry.registerComponent(appName, () => App);
