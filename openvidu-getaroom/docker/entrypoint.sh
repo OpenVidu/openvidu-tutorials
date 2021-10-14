ceb58d28bea3069f627d211d9eb3c08380e512cd
cruizba <carlos.ruizbal@gmail.com>
2021-10-15 00:47:13 +0200

    Fix OPENVIDU_URL in insecure docker tutorials OPENVIDU_URL is used properly in docker images serving nginx static insecure tutorials. Before, location.hostname was always used

--- openvidu-getaroom/docker/entrypoint.sh
+++ openvidu-getaroom/docker/entrypoint.sh
@@ -3,7 +3,7 @@
 [ ! -z "${OPENVIDU_URL}" ] && echo "OPENVIDU_URL: ${OPENVIDU_URL}" || echo "OPENVIDU_URL: default"
 [ ! -z "${OPENVIDU_SECRET}" ] && echo "OPENVIDU_SECRET: ${OPENVIDU_SECRET}" || echo "OPENVIDU_SECRET: default"
 
-sed -i "s/^var OPENVIDU_SERVER_URL =.*$/var OPENVIDU_SERVER_URL = \"$(echo https://\" + location.hostname | sed 's#/#\\/#g');/" /var/www/openvidu-getaroom/app.js
+sed -i "s|^var OPENVIDU_SERVER_URL =.*$|var OPENVIDU_SERVER_URL = \"${OPENVIDU_URL}\";|" /var/www/openvidu-getaroom/app.js
 
 if [ ! -z "${OPENVIDU_SECRET}" ]; then
     sed -i "s/^var OPENVIDU_SERVER_SECRET =.*$/var OPENVIDU_SERVER_SECRET = \"${OPENVIDU_SECRET}\";/" /var/www/openvidu-getaroom/app.js
