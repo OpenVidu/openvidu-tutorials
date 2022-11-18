c7a31d051b8a651be6df483c8a998b47e101a41a
Carlos Santos <4a.santos@gmail.com>
2022-11-18 12:27:00 +0100

    openvidu-components: Updated to Angular 14

--- openvidu-components/install.sh
+++ openvidu-components/install.sh
@@ -1,6 +1,21 @@
-TUTORIALS=('openvidu-custom-chat-panel' 'openvidu-custom-layout' 'openvidu-custom-panels' 'openvidu-custom-participant-panel-item'
-'openvidu-custom-participant-panel-item-elements' 'openvidu-custom-participants-panel' 'openvidu-custom-stream' 'openvidu-custom-toolbar'
-'openvidu-custom-toolbar' 'openvidu-custom-ui' 'openvidu-toggle-hand' 'openvidu-toolbar-buttons' 'openvidu-toolbar-panel-buttons')
+TUTORIALS=(
+  'openvidu-additional-panels'
+  'openvidu-admin-dashboard'
+  'openvidu-custom-activities-panel'
+  'openvidu-custom-chat-panel'
+  'openvidu-custom-layout'
+  'openvidu-custom-panels'
+  'openvidu-custom-participant-panel-item'
+  'openvidu-custom-participant-panel-item-elements'
+  'openvidu-custom-participants-panel'
+  'openvidu-custom-stream'
+  'openvidu-custom-toolbar'
+  'openvidu-custom-toolbar'
+  'openvidu-custom-ui'
+  'openvidu-toggle-hand'
+  'openvidu-toolbar-buttons'
+  'openvidu-toolbar-panel-buttons'
+)
 
 for tutorial in "${TUTORIALS[@]}"
 do
