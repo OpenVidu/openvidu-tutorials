0deb7cf716a50bca365de096bd5bba0ed6e6a733
Carlos Santos <4a.santos@gmail.com>
2024-10-24 09:34:32 +0200

    Updated dependencies

--- openvidu-components-angular/install.sh
+++ openvidu-components-angular/install.sh
@@ -2,21 +2,22 @@
 
 
 TUTORIALS=(
-  'openvidu-additional-panels'
-  'openvidu-admin-dashboard'
-  'openvidu-custom-activities-panel'
-  'openvidu-custom-chat-panel'
-  'openvidu-custom-layout'
-  'openvidu-custom-panels'
-  'openvidu-custom-participant-panel-item'
-  'openvidu-custom-participant-panel-item-elements'
-  'openvidu-custom-participants-panel'
-  'openvidu-custom-stream'
-  'openvidu-custom-toolbar'
-  'openvidu-custom-ui'
-  'openvidu-toggle-hand'
-  'openvidu-toolbar-buttons'
-  'openvidu-toolbar-panel-buttons'
+  # 'openvidu-additional-panels'
+  # 'openvidu-admin-dashboard'
+  # 'openvidu-custom-activities-panel'
+  # 'openvidu-custom-chat-panel'
+  # 'openvidu-custom-layout'
+  # 'openvidu-custom-panels'
+  # 'openvidu-custom-participant-panel-item'
+  # 'openvidu-custom-participant-panel-item-elements'
+  # 'openvidu-custom-participants-panel'
+  # 'openvidu-custom-stream'
+  # 'openvidu-custom-toolbar'
+  # 'openvidu-custom-ui'
+  # 'openvidu-toggle-hand'
+  # 'openvidu-toolbar-buttons'
+  # 'openvidu-toolbar-panel-buttons',
+  'openvidu-complete-app/frontend'
 )
 
 for tutorial in "${TUTORIALS[@]}"
@@ -24,7 +25,8 @@ do
   echo "Installing modules in $tutorial, please wait ..."
 
   cd $tutorial
-  rm package-lock.json && npm install
+  rm package-lock.json
+  npm i openvidu-components-angular@latest
   cd ..
 
 done
