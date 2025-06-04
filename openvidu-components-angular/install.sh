f91464e249f5a927046ce5516663a3ec6229baaf
cruizba <carlos.ruizbal@gmail.com>
2025-06-04 17:46:09 +0200

    Fix install script

--- openvidu-components-angular/install.sh
+++ openvidu-components-angular/install.sh
@@ -7,6 +7,7 @@ TUTORIALS=(
   'openvidu-complete-app/frontend'
   'openvidu-custom-activities-panel'
   'openvidu-custom-chat-panel'
+  'openvidu-custom-lang'
   'openvidu-custom-layout'
   'openvidu-custom-panels'
   'openvidu-custom-participant-panel-item'
@@ -17,16 +18,18 @@ TUTORIALS=(
   'openvidu-custom-ui'
   'openvidu-toggle-hand'
   'openvidu-toolbar-buttons'
-  'openvidu-toolbar-panel-buttons',
+  'openvidu-toolbar-panel-buttons'
 )
 
 for tutorial in "${TUTORIALS[@]}"
 do
   echo "Installing modules in $tutorial, please wait ..."
 
-  cd $tutorial
+  pushd "$tutorial" || exit 1
+
   # rm package-lock.json
+  npm version 3.2.0
   npm i openvidu-components-angular@latest
-  cd ..
+  popd || exit 1
 
 done
